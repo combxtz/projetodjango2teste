dc2af8fa1dc6d3189a30b0dea1e39128bbb85775
Kauan Matheus Gomes <kmsg@cesar.school>
2026-09-21 09:34:53 -0300

    Atualiza conexao dinamica de base de dados para PostgreSQL

--- setup/settings.py
+++ setup/settings.py
@@ -60,12 +60,22 @@ WSGI_APPLICATION = 'setup.wsgi.application'
 
 
 # Database
-DATABASES = {
-    'default': dj_database_url.config(
-        default='sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3'),
-        conn_max_age=600
-    )
-}
+DATABASE_URL = os.getenv('DATABASE_URL')
+
+if DATABASE_URL:
+    DATABASES = {
+        'default': dj_database_url.config(
+            default=DATABASE_URL,
+            conn_max_age=600
+        )
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation

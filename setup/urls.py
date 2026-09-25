179334fba41791a312230cacfc0b9d57bef15c28
combxtz <combathg72@gmail.com>
2026-09-25 19:48:34 -0300

    teste

--- setup/urls.py
+++ setup/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path , include
+from django.urls import path, include
+from hello import views as hello_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', hello_views.index),
     path("hello/", include("hello.urls")),
 ]
